a89b8627873c1afe90f020e92d925a8663410917
TomasGalera <143727579+TomasGalera@users.noreply.github.com>
2024-04-24 11:47:59 -0300

    Inicio punto 1

--- Front/dom.js
+++ Front/dom.js
@@ -1,18 +1,30 @@
 async function llamadoAlBackend() {
     //Llamo a un pedido Get del servidor
-    const response = await fetch('http://localhost:3000/saludo',{
+    const response = await fetch('http://localhost:3000/obtenerPilotos',{
         method:"GET",
         headers: {
             "Content-Type": "application/json",
           },
-    })
-
-    console.log(response)
+        })
+        
     //Tengo que usar el await porque la respuesta del servidor es lenta
     const result = await response.json()
     console.log(result)
-
-    document.getElementById("ejemplo").innerHTML = result.respuesta
+    
+    for (i in result){
+        document.getElementById("table").innerHTML += `
+        <tr>
+        <td>${result[i].nombre}</td>
+        <td>${result[i].apellido}</td>
+        <td>Germany</td>
+        </tr>
+        <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+        </tr>
+        `
+    }
 }
 
 async function envioPost() {
